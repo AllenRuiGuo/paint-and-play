57ee7d975d5ffdc86d83af97f5feaa9d24375906
Rui Guo <ruiguo.developer@outlook.com>
2026-08-09 15:55:50 +1000

    Edit Header and add logo

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -5,7 +5,13 @@ function Header() {
         <header className="site-header">
             <div className="nav">
                 <a href="#" className="logo">
-                    <div className="logo-icon">🎨</div>
+                    <div className="logo-icon">
+                        <img
+                            src="/logo.png"
+                            alt="Plaster Pals"
+                            className="logo-image"
+                        />
+                    </div>
                     <span>Plaster Pals</span>
                 </a>
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,12 @@
 import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
 
 function Home() {
     return (
         <>
             <Header />
+            <Hero />
 
-            <main>
-                <h1>Paint & Play</h1>
-            </main>
         </>
     );
 }
