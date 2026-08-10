76f1835469e1801a4e2371365d5469427ebe56d7
Rui Guo <ruiguo.developer@outlook.com>
2026-08-10 18:57:18 +1000

    Add Activities to home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,13 @@
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import Activities from "../components/Activities/Activities";
 
 function Home() {
     return (
         <>
             <Header />
             <Hero />
-
+            <Activities />
         </>
     );
 }
