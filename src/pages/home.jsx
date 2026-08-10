53554bd2410d22b95776b4422747725fd4728b6d
Rui Guo <ruiguo.developer@outlook.com>
2026-08-10 20:22:50 +1000

    Add BirthdayParties to home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import Activities from "../components/Activities/Activities";
 import Products from "../components/Products/Products";
+import BirthdayParties from "../components/BirthdayParties/BirthdayParties";
 
 function Home() {
     return (
@@ -10,6 +11,7 @@ function Home() {
             <Hero />
             <Activities />
             <Products />
+            <BirthdayParties />
         </>
     );
 }
