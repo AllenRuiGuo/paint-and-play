df01abd797bdf14aacce926fcc13ebc7c37456e7
Rui Guo <ruiguo.developer@outlook.com>
2026-08-10 20:15:54 +1000

    Add Products to home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import Activities from "../components/Activities/Activities";
+import Products from "../components/Products/Products";
 
 function Home() {
     return (
@@ -8,6 +9,7 @@ function Home() {
             <Header />
             <Hero />
             <Activities />
+            <Products />
         </>
     );
 }
