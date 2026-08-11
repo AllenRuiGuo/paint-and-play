2da58d93dc9ddb690ee851aeea42eb953b56441c
Rui Guo <ruiguo.developer@outlook.com>
2026-08-11 18:50:48 +1000

    Add Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,57 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer className="site-footer">
+            <div className="footer-container">
+
+                <div className="footer-brand">
+                    <a href="#" className="footer-logo">
+                        <img
+                            src="/logo.png"
+                            alt="Plaster Pals"
+                        />
+                    </a>
+
+                    <p>
+                        Creative fun for little artists.
+                    </p>
+                </div>
+
+
+                <div className="footer-links">
+                    <h3>Explore</h3>
+
+                    <a href="#activities">Activities</a>
+                    <a href="#products">Products</a>
+                    <a href="#parties">Birthday Parties</a>
+                    <a href="#vacation-care">Vacation Care</a>
+                </div>
+
+
+                <div className="footer-contact" id="contact">
+                    <h3>Contact</h3>
+
+                    <p>
+                        We'd love to hear from you!
+                    </p>
+
+                    <a href="mailto:hello@plasterpals.com.au">
+                        hello@plasterpals.com.au
+                    </a>
+                </div>
+
+            </div>
+
+
+            <div className="footer-bottom">
+                <p>
+                    © {new Date().getFullYear()} Plaster Pals. All rights reserved.
+                </p>
+            </div>
+
+        </footer>
+    );
+}
+
+export default Footer;
