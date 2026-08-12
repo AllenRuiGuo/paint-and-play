fd97997daa74424eb38bc4993c523ef88e05e289
Rui Guo <ruiguo.developer@outlook.com>
2026-08-12 10:08:58 +1000

    Control the modal from Home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,28 @@
+import { useState } from "react";
+
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import Activities from "../components/Activities/Activities";
 import Products from "../components/Products/Products";
 import BirthdayParties from "../components/BirthdayParties/BirthdayParties";
 import VacationCare from "../components/VacationCare/VacationCare";
+import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
 
 function Home() {
+    const [isContactOpen, setIsContactOpen] = useState(false); 
+    const openContact = () => { setIsContactOpen(true); }; 
+    const closeContact = () => { setIsContactOpen(false); };
+
     return (
         <>
-            <Header />
+            <Header onContactClick={openContact}/>
             <Hero />
             <Activities />
             <Products />
-            <BirthdayParties />
-            <VacationCare />
+            <BirthdayParties onContactClick={openContact}/>
+            <VacationCare onContactClick={openContact}/>
+            <Contact isOpen={isContactOpen} onClose={closeContact} />
             <Footer />
         </>
     );
